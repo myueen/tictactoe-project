a2ed10e86f395bd57ea27681815aeb71b2caf18b
YueEn Ma <myueen@unc.edu>
2023-08-29 09:51:58 -0400

    Progress in TicTacToe

---
+++ TicTacToe.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class TicTacToe {
+
+    public static void main(String[] arg){
+
+        char[][] gameBoard = {{'_', '_', '_'},
+                {'_', '_', '_'},
+                {'_', '_', '_'}};
+
+        System.out.println(gameBoard);
+
+
+
+    }
+
+
+}
